d41343beb5f2b743dbf32e670e291a114ab14a33
sanjeevsidana <sanjeevsidana@gmail.com>
2022-07-29 17:49:26 +0100

    rolled back

--- .devcontainer/scripts/gh.sh
+++ .devcontainer/scripts/gh.sh
@@ -15,8 +15,7 @@ echo -e "\e[34m»»» 📦 \e[32mInstalling \e[33m$NAME \e[35mv$VERSION\e[0m ...
 
 mkdir -p "$INSTALL_DIR"
 curl -sSL "https://github.com/cli/cli/releases/download/v${VERSION}/gh_${VERSION}_linux_amd64.tar.gz" -o /tmp/gh.tar.gz
-#tar -zxvf /tmp/gh.tar.gz --strip-components 2 -C "$INSTALL_DIR" "gh_${VERSION}_linux_amd64/bin/gh" > /dev/null
-tar -xvf /tmp/gh.tar.gz --strip-components 2 -C "$INSTALL_DIR" "gh_${VERSION}_linux_amd64/bin/gh" > /dev/null
+tar -zxvf /tmp/gh.tar.gz --strip-components 2 -C "$INSTALL_DIR" "gh_${VERSION}_linux_amd64/bin/gh" > /dev/null
 chmod +x "$INSTALL_DIR/gh"
 rm -rf /tmp/gh.tar.gz
 
